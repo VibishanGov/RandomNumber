3462137196df88fa477a2b2d17ce834c6f2d4850
Claire Lin <claire.nikita.lin@gmail.com>
2021-08-04 14:31:11 +0200

    Add randomOddNums.py

---
+++ randomOddNums.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import random
+
+def generateNumber():
+  number = random.randint(0, 99)
+  if number % 2 == 0:
+    number = number + 1
+  
+  return number
+
+if __name__ == "__main__":
+  print(generateNumber())
